5611119322a2f9d24e7fb7543a4bff34bd7c7303
Yankii01 <jean_antoni@hotmail.com>
2024-10-08 17:27:19 -0500

    V1.2 KLK GARGAAAAAAAAA

---
+++ SistemaPlanillas/src/test/java/pe/com/cibertec/SistemaPlanillasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPlanillasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
